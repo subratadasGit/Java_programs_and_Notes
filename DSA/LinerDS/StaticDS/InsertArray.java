c2f1b04011e5e6a93091941ed18882332253b9f5
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-11-15 11:11:46 +0000

    Array01

--- DSA/LinerDS/StaticDS/InsertArray.java
+++ DSA/LinerDS/StaticDS/InsertArray.java
@@ -1,6 +1,22 @@
-public class InsertArray {
+import java.util.*;
 
+public class InsertArray {
     public static void main(String[] args) {
-        int arr[] = }
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        System.out.println("Enter Your Elements:");
+        for (int i = 0; i < arr.length; i++) { // Insert Array values
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("The elements in the array are:");
+        for (int i = 0; i < arr.length; i++) { // Print Array values
+            System.out.print(arr[i] + "  ");
+        }
+        sc.close();
     }
 }
