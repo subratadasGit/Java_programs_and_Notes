5f2abbdfcea3d92e95076443146e1bb3473a1c4c
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-11-16 15:37:35 +0000

    insert at begining

--- DSA/LinerDS/StaticDS/InsertAtFirst.java
+++ DSA/LinerDS/StaticDS/InsertAtFirst.java
@@ -1,43 +1,38 @@
 import java.util.Scanner;
 
 public class InsertAtFirst {
-    
+
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the size of the array:");
+        System.out.println("Enter the size of the original array:");
         int size = sc.nextInt();
-        int arr[] = new int[size];
+        int[] arr = new int[size];
 
-        System.out.println("Enter Your Elements:");
-        for (int i = 0; i < arr.length; i++) { // Insert Array values
+        System.out.println("Enter your original elements:");
+        for (int i = 0; i < arr.length; i++) { // Insert original array values
             arr[i] = sc.nextInt();
         }
 
-        System.out.println("enter your new size array which you want to add at begining");
+        System.out.println("Enter the number of elements to add at the beginning:");
         int newsize = sc.nextInt();
-
         int[] newarr = new int[size + newsize];
 
-        System.out.println("Enter Your Elements:");
-        for (int i = 0; i < newsize ; i++) { // Insert new Array values
+        System.out.println("Enter the new elements:");
+        for (int i = 0; i < newsize; i++) { // Insert new array values
             newarr[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < size; i++) {            
-            newarr[newsize+i]=arr[i];
-
+        for (int i = 0; i < size; i++) { // Copy original array to new array
+            newarr[newsize + i] = arr[i];
         }
 
+        System.out.println("Array after inserting new elements at the beginning:");
         for (int i = 0; i < newarr.length; i++) {
-            System.out.print(newarr[i]+" ");
+            System.out.print(newarr[i] + " ");
         }
 
-
-
-
-        
-
+        sc.close();
     }
 }
